4a49da6c7b28c7d3d54de5139c0445f2b52f7b3b
Steven Tang <steven@steventang.net>
2022-02-01 21:39:49 +1100

    ref: move GET /heartbeats definition under compat

--- routes/api/heartbeat.go
+++ routes/api/heartbeat.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 	conf "github.com/muety/wakapi/config"
@@ -50,42 +49,6 @@ func (h *HeartbeatApiHandler) RegisterRoutes(router *mux.Router) {
 	r.Path("/v1/users/{user}/heartbeats.bulk").Methods(http.MethodPost).HandlerFunc(h.Post)
 	r.Path("/compat/wakatime/v1/users/{user}/heartbeats").Methods(http.MethodPost).HandlerFunc(h.Post)
 	r.Path("/compat/wakatime/v1/users/{user}/heartbeats.bulk").Methods(http.MethodPost).HandlerFunc(h.Post)
-	r.Path("/compat/wakatime/v1/users/{user}/heartbeats").Methods(http.MethodGet).HandlerFunc(h.Get)
-}
-
-// @Summary Get heartbeats of user for specified date
-// @ID get-heartbeats
-// @Tags heartbeat
-// @Param date query string true "Date"
-// @Param user path string true "Username (or current)"
-// @Security ApiKeyAuth
-// @Success 200
-// @Router /compat/wakatime/v1/users/{user}/heartbeats [get]
-func (h *HeartbeatApiHandler) Get(w http.ResponseWriter, r *http.Request) {
-	user, err := routeutils.CheckEffectiveUser(w, r, h.userSrvc, "current")
-	if err != nil {
-		return // response was already sent by util function
-	}
-
-	params := r.URL.Query()
-	dateParam := params.Get("date")
-	date, err := time.Parse(conf.SimpleDateFormat, dateParam)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("bad date"))
-		return
-	}
-	timezone := user.TZ()
-	rangeFrom, rangeTo := utils.StartOfDay(date).In(timezone), utils.EndOfDay(date).In(timezone)
-
-	heartbeats, err := h.heartbeatSrvc.GetAllWithin(rangeFrom, rangeTo, user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(conf.ErrInternalServerError))
-		conf.Log().Request(r).Error("failed to retrieve heartbeats - %v", err)
-		return
-	}
-	utils.RespondJSON(w, r, http.StatusOK, heartbeats)
 }
 
 // @Summary Push a new heartbeat
